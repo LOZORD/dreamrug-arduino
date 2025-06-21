5e8ba03d0e9ca70391aa73af8d79edff96d11a20
Leo Rudberg <ljr@google.com>
2025-06-20 23:47:15 -0400

    Configure all 8 default Nano inputs.

--- dreamrug-arduino.ino
+++ dreamrug-arduino.ino
@@ -3,8 +3,12 @@
 const int NO_MULTIPLEXER = -1;
 
 typedef struct {
+  // If non-negative, the multiplexer pin to read from.
   int multiplexerPin;
+  // The analog pin to read from.
+  // If the multiplexer pin is present, this is the pin on the multiplexer to use.
   int analogPin;
+  // The arbitrary ID assigned to this input for use by downstream programs.
   int id;
 } SensorInput;
 
@@ -21,6 +25,26 @@ const SensorInput SENSOR_INPUTS[] = {{
   .multiplexerPin = NO_MULTIPLEXER,
   .analogPin = 3,
   .id = 1003,
+}, {
+  .multiplexerPin = NO_MULTIPLEXER,
+  .analogPin = 4,
+  .id = 1004,
+}, {
+  .multiplexerPin = NO_MULTIPLEXER,
+  .analogPin = 5,
+  .id = 1005
+}, {
+  .multiplexerPin = NO_MULTIPLEXER,
+  .analogPin = 6,
+  .id = 1006,
+}, {
+  .multiplexerPin = NO_MULTIPLEXER,
+  .analogPin = 7,
+  .id = 1007,
+}, {
+  .multiplexerPin = NO_MULTIPLEXER,
+  .analogPin = 0,
+  .id = 1000,
 }};
 const int NUM_SENSOR_INPUTS = sizeof(SENSOR_INPUTS) / sizeof(SensorInput);
 const int LOOP_DELAY_MILLIS = 100;
@@ -34,12 +58,14 @@ typedef struct {
   int readingValue;
 } InputReading;
 
-
 void loop() {
   InputReading readings[NUM_SENSOR_INPUTS];
   for (int i = 0; i < NUM_SENSOR_INPUTS; i++) {
     SensorInput input = SENSOR_INPUTS[i];
-    // TODO: Handle reading from multiplexers.
+    if (input.multiplexerPin >= 0) {
+      // TODO: Handle reading from multiplexers.
+      continue;
+    }
     int value = analogRead(input.analogPin);
     readings[i] = InputReading{
       .fromInput = input,
